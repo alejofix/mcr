e667bbacb00bf11e299461b66f72775f536b8cb8
AlejoFix <alejo_fix@HOTMAIL.COM>
2017-04-03 19:32:52 -0500

    Mejoras Views para Usuarios Agregar II

--- src/Fix/ServicemeBundle/Form/UsuariosType.php
+++ src/Fix/ServicemeBundle/Form/UsuariosType.php
@@ -11,7 +11,10 @@ use \Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use \Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use \Symfony\Component\Form\Extension\Core\Type\DateType;
 use \Symfony\Component\Form\Extension\Core\Type\SubmitType; 
+use \Symfony\Component\Form\Extension\Core\Type\BirthdayType;
 use \Symfony\Component\Form\Extension\Core\Type\TextType;
+
+use \Symfony\Component\Validator\Constraints;
 
 
 class UsuariosType extends AbstractType
@@ -22,6 +25,12 @@ class UsuariosType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
+   #         ->add('usuario', TextType::class, 
+#                    array('label' => 'Usuario',
+#                          'constraints' => array(
+#                          new Constraints\NotBlank(array('message' => 'Debe ingresar el Usuario')),
+#                            )
+#                        ))
             ->add('usuario', TextType::class)
             ->add('nombres', TextType::class)
             ->add('apellidos', TextType::class)
@@ -30,19 +39,28 @@ class UsuariosType extends AbstractType
                         'Femenino' => '0',
                         'Masculino' => '1',
                     ), 'placeholder' => 'Seleccione una Opción'))
-            ->add('fechaNacimiento', DateType::class, array('label' => 'Fecha Nacimiento'))
+            ->add('fechaNacimiento', BirthdayType::class, array(
+                            'placeholder' => array(
+                            'year' => 'Año', 'month' => 'Mes', 'day' => 'Día',)
+                            ))
             ->add('correo', EmailType::class)
             ->add('contrasena', PasswordType::class, array('label' => 'Contraseña'))
-            ->add('documentoTipo', TextType::class, array('label' => 'Tipo Documento'))
+            ->add('documentoTipo', ChoiceType::class,
+                    array( 'choices' => array(
+                        'Cédula de Ciudadanía' => 'CC',
+                        'Tarjeta de identidad' => 'TI',
+                        'Cédula de Extranjería' => 'CE'
+                    ), 'placeholder' => 'Seleccione una Opción')
+                )
             ->add('documentoNumero', TextType::class, array('label' => 'Número Documento'))
             ->add('telefonoFijo', TextType::class, array('label' => 'Teléfono Fijo'))
             ->add('telefonoMovil', TextType::class, array('label' => 'Teléfono Móvil'))
             ->add('cargo', TextType::class)
             ->add('role', ChoiceType::class, 
                     array('choices' => array(
-                        'ROLE_ADMIN' => 'Administrador',
-                        'ROLE_MCR' => 'Analista',
-                        'ROLE_INV' => 'Invitado'
+                        'Administrador' => 'ROLE_ADMIN',
+                        'Analista' => 'ROLE_MCR',
+                        'Invitado' => 'ROLE_INV'
                     ), 'placeholder' => 'Seleccione una Opción')
                  )
             ->add('activo')
