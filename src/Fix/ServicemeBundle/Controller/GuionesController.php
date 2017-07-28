27d92d0c59c592d513657e6f596f6f8a74aa55e0
alejofix <alejo_fix@hotmail.com>
2017-07-28 15:41:22 -0500

    ADD Resources Views guiones Plataforma

--- src/Fix/ServicemeBundle/Controller/GuionesController.php
+++ src/Fix/ServicemeBundle/Controller/GuionesController.php
@@ -18,7 +18,7 @@ class GuionesController extends Controller
      * GuionesController::copiarAction()
      *
      * @return
-     * @Route(path="/copiar", name="copiar")
+     * @Route(path="/copiar", name="copiarGuion")
      * @Template("FixServicemeBundle:Guiones:copiar.html.twig")
      */
     public function copiarAction()
@@ -26,5 +26,69 @@ class GuionesController extends Controller
 
 
     }
+
+    /**
+     * GuionesController::plataformadiagnosticadorAction()
+     *
+     * @return
+     * @Route(path="/plataforma/diagnosticador", name="plataformaDiagnosticador")
+     * @Template("FixServicemeBundle:Guiones:Plataforma/diagnosticador.html.twig")
+     */
+    public function plataformadiagnosticadorAction()
+    {
+
+    }
+
+    /**
+     * GuionesController::plataformageneralesAction()
+     *
+     * @return
+     * @Route(path="/plataforma/generales", name="plataformaGenerales")
+     * @Template("FixServicemeBundle:Guiones:Plataforma/generales.html.twig")
+     */
+    public function plataformageneralesAction()
+    {
+
+    }
+
+    /**
+     * GuionesController::plataformainternetAction()
+     *
+     * @return
+     * @Route(path="/plataforma/internet", name="plataformaInternet")
+     * @Template("FixServicemeBundle:Guiones:Plataforma/internet.html.twig")
+     */
+    public function plataformainternetAction()
+    {
+
+    }
+
+    /**
+     * GuionesController::plataformatelevisionAction()
+     *
+     * @return
+     * @Route(path="/plataforma/television", name="plataformaTelevision")
+     * @Template("FixServicemeBundle:Guiones:Plataforma/television.html.twig")
+     */
+    public function plataformatelevisionAction()
+    {
+
+    }
+
+    /**
+     * GuionesController::plataformatelefoniaAction()
+     *
+     * @return
+     * @Route(path="/plataforma/telefonia", name="plataformaTelefonia")
+     * @Template("FixServicemeBundle:Guiones:Plataforma/telefonia.html.twig")
+     */
+    public function plataformatelefoniaAction()
+    {
+
+    }
+
+
+
+
 
 }
